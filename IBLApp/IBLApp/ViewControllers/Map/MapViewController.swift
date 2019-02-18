12c0f1d1a35bcc8a4bde555bd86aac75b19fe571
Visar Ibishi <visar@appspeople.com.au>
2019-02-18 18:57:20 +0100

    Added Map storyboard Added MapViewController

---
+++ IBLApp/IBLApp/ViewControllers/Map/MapViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MapViewController.swift
+//  IBLApp
+//
+//  Created by Visar on 2/18/19.
+//  Copyright © 2019 Visar. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
