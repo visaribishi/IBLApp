04af77c68623aedd1983dec6fe64211b27fc0f05
Visar Ibishi <visar@appspeople.com.au>
2019-02-18 18:53:08 +0100

    Modified Main to match the design Added code to add/remove childViewControllers

--- IBLApp/IBLApp/ViewControllers/ViewController.swift
+++ IBLApp/IBLApp/ViewControllers/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var containerView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func myLocationButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func locationButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func listButtonPressed(_ sender: Any) {
+    }
+    
+    private func add(asChildViewController viewController: UIViewController) {
+        // Add Child View Controller
+        addChild(viewController)
+        
+        // Configure Child View
+        viewController.view.frame = containerView.bounds
+        viewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        // Add Child View as Subview
+        containerView.addSubview(viewController.view)
+        
+        // Notify Child View Controller
+        viewController.didMove(toParent: self)
+    }
+    
+    private func remove(asChildViewController viewController: UIViewController) {
+        // Notify Child View Controller
+        viewController.willMove(toParent: nil)
+        
+        // Remove Child View From Superview
+        viewController.view.removeFromSuperview()
+        
+        // Notify Child View Controller
+        viewController.removeFromParent()
+    }
 
 }
 
