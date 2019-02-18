045b7e9d1aec553752b310b87fdfd6ac4af89d3c
Visar Ibishi <visar@appspeople.com.au>
2019-02-18 19:06:30 +0100

    Added List storyboard Added ListViewController Added ListTableViewCell

---
+++ IBLApp/IBLApp/ViewControllers/List/Cells/ListTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ListTableViewCell.swift
+//  IBLApp
+//
+//  Created by Visar on 2/18/19.
+//  Copyright © 2019 Visar. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var typeImageView: UIImageView!
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ IBLApp/IBLApp/ViewControllers/List/ListViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ListViewController.swift
+//  IBLApp
+//
+//  Created by Visar on 2/18/19.
+//  Copyright © 2019 Visar. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
