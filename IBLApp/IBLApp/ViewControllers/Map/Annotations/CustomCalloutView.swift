b6f6acee384a26975688eede9dbb9f285ffb270b
Visar Ibishi <visar@appspeople.com.au>
2019-02-19 19:23:46 +0100

    Created CustomCalloutView

---
+++ IBLApp/IBLApp/ViewControllers/Map/Annotations/CustomCalloutView.swift
@@ -0,0 +1,19 @@
+//
+//  CustomCalloutView.swift
+//  IBLApp
+//
+//  Created by Visar on 2/19/19.
+//  Copyright © 2019 Visar. All rights reserved.
+//
+
+import UIKit
+
+class CustomCalloutView: UIView {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
