220ca296e8b33c7dddd8fc735ab70727fe0692b0
Visar Ibishi <visar@appspeople.com.au>
2019-02-20 11:48:43 +0100

    Added IBOutlets in DescriptionViewController annd filled them with data Passed the object to DetailsTableViewController

--- IBLApp/IBLApp/ViewControllers/Map/Details/DetailsViewController.swift
+++ IBLApp/IBLApp/ViewControllers/Map/Details/DetailsViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class DetailsViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
     var bank: Bank!
 
     override func viewDidLoad() {
@@ -17,20 +19,23 @@ class DetailsViewController: UIViewController {
         self.navigationItem.title = "Location"
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "ic_back"), style: .plain, target: self, action: #selector(navigateBack))
         self.navigationItem.leftBarButtonItem?.tintColor = .white
+        nameLabel.text = bank.name
+        addressLabel.text = bank.address
     }
     
     @objc func navigateBack() {
         self.navigationController?.popViewController(animated: true)
     }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if let destination = segue.destination as? DetailsTableViewController {
+            destination.bank = self.bank
+        }
     }
-    */
+ 
 
 }
